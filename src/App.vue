1cfd72727891761ff9183c9e7d7605ae88bed5c4
coderjc <2740954762@qq.com>
2024-09-03 12:18:47 +0800

    feat: init editor

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup>
 import Editor from './package/editor'
+import data from './data'
+import { ref } from 'vue'
+
+const state = ref(data)
 </script>
 
 <template>
-	<Editor></Editor>
+	<div class="container">
+		<Editor v-model="state"></Editor>
+	</div>
 </template>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.container {
+	padding: 20px;
+	position: fixed;
+	inset: 0;
+	overflow: hidden;
+}
+</style>
--- src/package/editor.jsx
+++ src/package/editor.jsx
@@ -1,11 +1,46 @@
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import './editor.less'
 
 export default defineComponent({
     name: 'Editor',
-    setup(){
+    props:{
+        modelValue:{
+            typeo:Object,
+            default:()=>({})
+        }
+    },
+    setup(props,{emit}){
+        const modelValue = computed({
+            get:()=>props.modelValue,
+            set:(val)=>emit('update:modelValue',val)
+        })
+
+        const containerStyle = computed(()=>{
+            return {
+                width:modelValue.value.container.width + 'px',
+                height:modelValue.value.container.height+ 'px'
+            }
+        })
+
         return ()=><>
         <div class="editor-container">
-            <h1>编辑器</h1>
+            {/* 物料 */}
+            <div class="editor-material"></div>
+            {/* 主区域 */}
+            <div class="editor-main">
+                {/* 菜单 */}
+                <div class="editor-menu"></div>
+                {/* 内容 */}
+                <div class="editor-content">
+                    {/* 画布包 */}
+                    <div class="editor-canvas-wrap">
+                        {/* 画布内容 */}
+                        <div style={containerStyle.value} class="editor-canvas"></div>
+                    </div>
+                </div>
+            </div>
+            {/* 控制 */}
+            <div class="editor-controller"></div>
         </div>
         </>
     }
